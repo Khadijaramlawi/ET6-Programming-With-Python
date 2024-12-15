aba2f3a679c57831b728eff6751bdf72b4358142
Khadija Ramlawi <kalramlawi@gmail.com>
2024-12-15 01:56:01 -0800

    first try for adding unittests to mystery 8

--- 3_documenting_and_testing/exercises/tests/test_mystery_8.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_8.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ..mystery_8 import mystery_8
+from .. mystery_8 import mystery_8
 
 class TestMystery8(unittest.TestCase):
     """ """
@@ -8,3 +8,28 @@ class TestMystery8(unittest.TestCase):
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_8([], ''), [])
+
+
+    def test_unordered_numbers(self):
+
+        self.assertEqual(mystery_8([4,6,2,9,1],6), [6])
+    
+    def test_characters(self):
+        
+        self.assertEqual(mystery_8(['k','h','a','d','i','j','a'],'d'), ['d'])
+    
+    def test_characters2(self):
+        
+        self.assertEqual(mystery_8(['k','h','a','d','d','i','j','a'],'d'), ['d','d'])
+    
+    def test_characters3(self):
+        
+        self.assertEqual(mystery_8(['k','h','a','d','d','i','j','a'],''), ['k', 'h', 'a', 'd', 'd', 'i', 'j', 'a'])
+        
+    def test_characters4(self):
+        
+        self.assertEqual(mystery_8(['c','a','t'],'t'), ['t'])
+        
+    def test_words(self):
+        
+        self.assertEqual(mystery_8(['bird','cat','dog'],'cat'), ['cat'])
